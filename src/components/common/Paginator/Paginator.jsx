0539a2a9ad3e5b1d7ac8872c56704bba54a1a639
SircoGeji <semenkov777@gmail.com>
2020-09-21 11:17:24 +0300

    New pages style

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -8,18 +8,42 @@ let Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged, ...prop
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
-    ;
-
-    return <div>
+    /*{
         {pages.map(p => {
             return (
                 <span
                     className={currentPage === p && styles.selectedPage}
                     onClick={() => {
                         onPageChanged(p)
-                    }}>{p} </span>
+                    }}>{p + " "}
+                </span>
             )
         })}
+    }*/
+    return <div className={styles.pages}>
+        {
+            (currentPage - 1)
+                ? <button
+                    onClick={() => {
+                        onPageChanged(currentPage - 1)
+                    }}
+                >Previous</button>
+                : null
+        }
+
+        <div>
+            ...
+        </div>
+        {
+            (currentPage + 1)
+                ? <button
+                    onClick={() => {
+                        onPageChanged((currentPage + 1) ? currentPage + 1 : currentPage)
+                    }}
+                >Next</button>
+                : null
+        }
+
     </div>
 }
 
